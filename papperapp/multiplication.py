7e971c0ee5dfb1803c4bd94eb487d98b8004ef44
efriden <eric.friden@gmail.com>
2020-03-21 21:08:00 +0100

    Tog med allt som låg lokalt på siri

--- papperapp/multiplication.py
+++ papperapp/multiplication.py
@@ -1,28 +1,50 @@
 import random
 
 def generate_mult_exercises(n):
-	result = []
+	problems = []
+	answers = []
 	for i in range(n):
-		result.append(generate_mult_triple())
-	return result
+		triple_problems = []
+		triple_answers = []
+		a = random.randint(2,9)
+		b = random.randint(2,9)
+		for i in range(3):
+			res = generate_mult(a,b,i)
+			triple_problems.append(res[0])
+			triple_answers.append(res[1])
+		problems.append(triple_problems)
+		answers.append(triple_answers)
+	return [problems, answers]
 
-def generate_mult_triple():
-	result = []
-	a = random.randint(2,9)
-	b = random.randint(2,9)
-	for i in range(3):
-		result.append(generate_mult(a,b,i)[0])
-	return result
+def generate_mult_problems(n):
+	problems = []
+	answers = []
+	for i in range(n):
+		triple_problems = []
+		triple_answers = []
+		for i in range(3):
+			a = random.randint(2,9)
+			b = random.randint(2,9)
+			res = generate_mult(a,b,2)
+			triple_problems.append(res[0])
+			triple_answers.append(res[1])
+		problems.append(triple_problems)
+		answers.append(triple_answers)
+	return [problems, answers]
 
 def generate_mult_example(a,b):
 	result = []
 	for i in range(3):
-		result.append(generate_mult(a,b,i))
+		result.append(generate_mult(a,b,i,False))
 	return result
 
-def generate_mult(a, b, i):
-	new_b = round(b*(10**(-i)),i)
-	ans = round(a*new_b,i)
+def generate_mult(a, b, i, random_exp = True):
+	if (i == 2 and random_exp):
+		exp = random.randint(2,4)
+	else:
+		exp = i
+	new_b = round(b*(10**(-exp)),exp)
+	ans = round(a*new_b,exp)
 	return [str(a) + "\cdot" +  swedify(new_b),
 		 swedify(ans)]
 
--- papperapp/views.py
+++ papperapp/views.py
@@ -8,69 +8,109 @@ from django.http import FileResponse
 from reportlab.pdfgen import canvas
 import random
 
-from django_tex.shortcuts import render_to_pdf
+from PyPDF2 import PdfFileMerger, PdfFileReader
+
+from django_tex.shortcuts import render_to_pdf, compile_template_to_pdf
 
 import papperapp.multiplication as m
+
+#ToDo: set up jinja template inheritance
 
 #######
 #VIEWS:
 #######
-def test(request):
-    template_name = 'tex/test.tex'
-    context = {'foo': 'Bar'}
-    return render_to_pdf(request, template_name, context, filename='test.pdf')
-
-def fractions(request):
-	template_name = 'tex/fractions.tex'
-	context = {
-		'exercise_name': 'BRÅK MED DECIMAL',
-		'student_name': 'Elev Testson',
-		'numbers': generate_exercises(3),
-		}
-	for key in context:
-		if (type(context[key]) == str):
-			context[key] = tex_escape(context[key])
-	return render_to_pdf(request, template_name, context, filename='test.pdf')
 
 def mult(request):
 	template_name = 'tex/mult.tex'
-	context = {
-		'exercise_name': 'MULTIPLIKATION MED DECIMAL',
-		'student_name': 'Elev Testson',
-		'ex': m.generate_mult_example(3, 2),
-		'q': m.generate_mult_exercises(3),
-		}
-	for key in context:
-		if (type(context[key]) == str):
-			context[key] = tex_escape(context[key])
-	return render_to_pdf(request, template_name, context, filename='test.pdf')
+	student_names = ['Elev Testson1','Elev Testson2','Elev Testson3']
+	exercise_name = "MULTIPLIKATION MED DECIMAL"
+
+	pdfs = []
+	answers = {}
+
+	for student_name in student_names:
+		questions = m.generate_mult_exercises(4)
+		problems = m.generate_mult_problems(2)
+		answers[student_name] = questions[1] + problems[1]
+		context = {
+			'exercise_name': exercise_name,
+			'student_name': student_name,
+			'ex': m.generate_mult_example(3, 2),
+			'q': questions[0],
+			'p': problems[0],
+			}
+		for key in context:
+			if (type(context[key]) == str):
+				context[key] = tex_escape(context[key])
+
+		buffer = io.BytesIO(compile_template_to_pdf(template_name, context))
+
+		pdfs.append(buffer)
+
+	#answer_context = ""
+	#for student_name in answers:
+	#	answer_context += "\section*{" + student_name + "} "
+	#	answer_context += "\begin{wrapfigure} "
+		#todo - make it a list of inputs and iterate with jinja (% for .... %) or whatever
+
+
+
+
+		#for i in range(len(answers[student_name])):
+		#	answer_context += str(i+2)
+		#	answer_context += ":"
+
+		#	letters = ["a", "b", "c"]
+		#	for j in range(3):
+		#		answer_context += letters[j]
+		#		answer_context += ")"
+		#		answer_context += answers[student_name][i][j]
+		#		answer_context += " "
+
+		#	answer_context += "\\\\ "
+
+		#answer_context += "\end{wrapfigure}"
+
+	#answer_context = tex_escape(answer_context)
+
+
+
+	answer_page = io.BytesIO(compile_template_to_pdf("tex/answers.tex", {"content": answers, "exercise_name": exercise_name}))
+
+	merger = PdfFileMerger()
+
+	for pdf in pdfs:
+		merger.append(PdfFileReader(pdf))
+
+	merger.append(PdfFileReader(answer_page))
+
+	output_buffer = io.BytesIO()
+
+	merger.write(output_buffer)
+
+	http_response = HttpResponse(output_buffer.getvalue() , content_type='application/pdf')
+	http_response['Content-Disposition'] = 'filename="mattepapper.pdf"'
+
+	return http_response
+
+
+
+# def fractions(request):
+# 	template_name = 'tex/fractions.tex'
+# 	context = {
+# 		'exercise_name': 'BRÅK MED DECIMAL',
+# 		'student_name': 'Elev Testson',
+# 		'numbers': generate_exercises(3),
+# 		}
+# 	for key in context:
+# 		if (type(context[key]) == str):
+# 			context[key] = tex_escape(context[key])
+# 	return render_to_pdf(request, template_name, context, filename='test.pdf')
+
 
 ###########
 #FUNCTIONS:
 ###########
-def generate_exercises(n):
-	result = []
-	for i in range(n):
-		result.append(generate_fraction_triple())
-	return result
-
-
-
-def generate_fraction_triple():
-	results = []
-	for i in range(3):
-		results.append(generate_fraction())
-	return results
-
-
-
-def generate_fraction():
-	a = random.randint(2,9)
-	b = 0.1
-	c = int(a/b)
-	return [str(a), swedify(b), swedify(c)]
-
-
 
 def tex_escape(text):
     """
