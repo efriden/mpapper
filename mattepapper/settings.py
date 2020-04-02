5002b81c36070b946d0aa5cfb8694de5aa862ece
efriden <eric.friden@gmail.com>
2020-04-02 23:24:17 +0200

    mer test för static

--- mattepapper/settings.py
+++ mattepapper/settings.py
@@ -143,8 +143,8 @@ STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 LATEX_INTERPRETER = 'pdflatex'
