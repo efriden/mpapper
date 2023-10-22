ecb1577e94516ed2cd5ea978da445f05081117e1
efriden <44654018+efriden@users.noreply.github.com>
2023-10-22 17:32:08 +0200

    Update urls.py

--- mattepapper/urls.py
+++ mattepapper/urls.py
@@ -25,24 +25,7 @@ urlpatterns = [
     #eller är det en dålig idé? Ska skillnaden mellan olika dok ligga i urlpatterns eller i views?
     url(r'^contact$', app.views.contact, name='contact'),
     url(r'^about$', app.views.about, name='about'),
-    url(r'^login/$',
-        django.contrib.auth.views.login,
-        {
-            'template_name': 'app/login.html',
-            'authentication_form': app.forms.BootstrapAuthenticationForm,
-            'extra_context':
-            {
-                'title': 'Log in',
-                'year': datetime.now().year,
-            }
-        },
-        name='login'),
-    url(r'^logout$',
-        django.contrib.auth.views.logout,
-        {
-            'next_page': '/',
-        },
-        name='logout'),
+    
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
